985c06a80e5a92db0af5b6ed89f66ca6484ab217
Filipe Alves De Souza <filipewallace19@gmail.com>
2022-06-22 12:03:24 -0400

    create Boot REST project, set JPA project dependency

---
+++ MythicMod/src/test/java/com/skilldistillery/mod/MythicModApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.mod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MythicModApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
