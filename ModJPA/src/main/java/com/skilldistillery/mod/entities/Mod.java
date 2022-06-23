aab9ff6423aaa14c853c9e0bb971e4163a09dc77
Filipe Alves De Souza <filipewallace19@gmail.com>
2022-06-23 11:56:41 -0400

    fixed mod entity and junit

--- ModJPA/src/main/java/com/skilldistillery/mod/entities/Mod.java
+++ ModJPA/src/main/java/com/skilldistillery/mod/entities/Mod.java
@@ -8,11 +8,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 @Entity
+@Table(name="game_mod")
 public class Mod {
 
 	@Id
