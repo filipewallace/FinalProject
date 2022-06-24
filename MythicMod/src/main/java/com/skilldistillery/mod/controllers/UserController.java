a9d86487d2b0282616e380937fffe6b8e079e5f9
Henrymelendez <henryomarm@gmail.com>
2022-06-24 12:16:00 -0400

    Added user controller

--- MythicMod/src/main/java/com/skilldistillery/mod/controllers/UserController.java
+++ MythicMod/src/main/java/com/skilldistillery/mod/controllers/UserController.java
@@ -1,13 +1,17 @@
 package com.skilldistillery.mod.controllers;
 
+import java.security.Principal;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,6 +42,11 @@ public class UserController {
 	
 //	------------------------new----------------------------------------------------------
 	
+	
+	
+	
+	
+	
 	@PostMapping("user")
 	public User addNewUser(
 			
@@ -59,4 +68,49 @@ public class UserController {
 		}
 		return user;
 	}
+	
+	
+	@DeleteMapping("user/{id}")
+	public void deleteUser(@RequestBody User user,
+			HttpServletRequest req,
+			HttpServletResponse res, @PathVariable int id, Principal principal) {
+		if(userService.destroyUser(principal.getName(),id)) {
+			res.setStatus(201);
+		} else {
+			res.setStatus(400);
+		}				
+		
+	}
+	
+	@PutMapping("user/{id}")
+	public User update(HttpServletResponse res, HttpServletRequest req, @PathVariable int id, Principal principal, @RequestBody User user) {
+		user = userService.updateUser(principal.getName(), id, user );
+		if(user == null) {
+			res.setStatus(400);
+		}
+		else {
+			res.setStatus(201);
+		}
+		
+		return user;
+		
+	}
+	
+	
+	@GetMapping("user/{id}")
+	public User Show(HttpServletRequest req, HttpServletResponse res, @PathVariable int id, Principal principal) {
+		User user = userService.showUser(principal.getName(), id);
+		if(user == null) {
+			res.setStatus(404);
+		}
+		return user;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 }
