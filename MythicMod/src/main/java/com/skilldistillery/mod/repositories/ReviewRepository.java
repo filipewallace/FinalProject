85e644bdbea0575bde2fcd7d300a595e3863d639
Henrymelendez <henryomarm@gmail.com>
2022-06-24 13:47:17 -0400

    Added Review Repo and Sevice and impl

---
+++ MythicMod/src/main/java/com/skilldistillery/mod/repositories/ReviewRepository.java
@@ -0,0 +1,15 @@
+package com.skilldistillery.mod.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.skilldistillery.mod.entities.Review;
+
+public interface ReviewRepository extends JpaRepository<Review, Integer> {
+	
+	List<Review> findByUser_Username(String username);
+	
+	Review findByIdAndUser_Username(int id, String username);
+
+}
