fcb26b3a95c9196865361385a4c73cd4404056ef
Henrymelendez <henryomarm@gmail.com>
2022-06-24 17:03:31 -0400

    Added Category service and impl

---
+++ MythicMod/src/main/java/com/skilldistillery/mod/repositories/ModMediaRepository.java
@@ -0,0 +1,16 @@
+package com.skilldistillery.mod.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.skilldistillery.mod.entities.ModMedia;
+
+public interface ModMediaRepository extends JpaRepository<ModMedia, Integer> {
+	
+	List<ModMedia> findByUser_Username(String username);
+	
+	ModMedia findByIdAndUser_Username(int id, String username);
+	
+
+}
---
+++ MythicMod/src/main/java/com/skilldistillery/mod/services/CategoryServiceImpl.java
@@ -0,0 +1,61 @@
+package com.skilldistillery.mod.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.skilldistillery.mod.entities.Category;
+import com.skilldistillery.mod.repositories.CategoryRepository;
+
+public class CategoryServiceImpl implements CategoryService {
+	
+	@Autowired
+	private CategoryRepository catRepo;
+
+	@Override
+	public List<Category> index() {
+		
+		return catRepo.findAll();
+	}
+
+	@Override
+	public Category show(int id) {
+		
+		return catRepo.findById(id);
+	}
+
+	@Override
+	public Category create(Category category) {
+		catRepo.saveAndFlush(category);
+		
+		
+		
+		
+		return category;
+	}
+
+	@Override
+	public Category update(int id, Category category) {
+		// TODO Auto-generated method stub
+		Category managed = catRepo.findById(id);
+		if(managed != null) {
+			managed.setGenre(category.getGenre());
+			managed.setId(id);
+			
+			catRepo.saveAndFlush(managed);
+			return managed;
+		}
+		return null;
+	}
+
+	@Override
+	public boolean destroy(int id) {
+		// TODO Auto-generated method stub
+		if(catRepo.findById(id) != null) {
+			catRepo.deleteById(id);
+			return true;
+		}
+		return false;
+	}
+
+}
