c3fd14479170387e9018602f9b136ffa55abbac6
Filipe Alves De Souza <filipewallace19@gmail.com>
2022-06-25 12:32:25 -0400

    copied usercontroller from github directly to try to solve git issues

--- MythicMod/src/main/java/com/skilldistillery/mod/controllers/UserController.java
+++ MythicMod/src/main/java/com/skilldistillery/mod/controllers/UserController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,7 +27,7 @@ public class UserController {
 
 	@Autowired
 	private UserService userService;
-	
+
 	// SMOKE TEST ONLY, DELETE/COMMENT OUT LATER
 	/*
 	 * @GetMapping("test/users/{userId}") public User getUserForTest(
@@ -35,60 +36,73 @@ public class UserController {
 	 * userService.getUserById(userId); if (user == null) { res.setStatus(404); }
 	 * return user; }
 	 */
-	
+
 //	------------------------new----------------------------------------------------------
+
+	@DeleteMapping("user/{id}")
+	public void deleteUser(HttpServletRequest req, HttpServletResponse res, @PathVariable int id) {
+		try {
+			if (userService.destroyUser(id)) {
+				res.setStatus(204);
+			} else {
+				res.setStatus(404);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			res.setStatus(400);
+		}
+
+	}
+
+	@PutMapping("user/{id}")
+	public User update(HttpServletResponse res, HttpServletRequest req, @PathVariable int id, Principal principal,
+			@RequestBody User user) {
+		user = userService.updateUser(id, user);
+		if (user == null) {
+			res.setStatus(400);
+		} else {
+			res.setStatus(201);
+		}
+
+		return user;
+
+	}
 	
 	
-	
-	
+	@PostMapping("user")
+	public User createNewUser(HttpServletRequest req, HttpServletResponse res, @RequestBody User user,
+			Principal principal) {
+
+		try {
+			user = userService.createUser(user);
+			if (user == null) {
+
+				res.setStatus(404);
+			} else {
+				res.setStatus(201);
+				StringBuffer url = req.getRequestURL();
+				url.append("/").append(user.getId());
+				res.setHeader("Location", url.toString());
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.println("Invalid Todo JSON");
+			res.setStatus(400);
+			user = null;
+		}
+		return user;
+	}
 	
 	
 
-	
-	
-	@DeleteMapping("user/{id}")
-	public void deleteUser(
-			HttpServletRequest req,
-			HttpServletResponse res, @PathVariable int id) {
-		if(userService.destroyUser(id)) {
-			res.setStatus(201);
-		} else {
-			res.setStatus(400);
-		}				
-		
-	}
-	
-	@PutMapping("user/{id}")
-	public User update(HttpServletResponse res, HttpServletRequest req, @PathVariable int id, Principal principal, @RequestBody User user) {
-		user = userService.updateUser(id, user );
-		if(user == null) {
-			res.setStatus(400);
-		}
-		else {
-			res.setStatus(201);
-		}
-		
-		return user;
-		
-	}
-	
-	
 	@GetMapping("user/{id}")
 	public User Show(HttpServletRequest req, HttpServletResponse res, @PathVariable int id, Principal principal) {
 		User user = userService.showUser(id);
-		if(user == null) {
+		if (user == null) {
 			res.setStatus(404);
 		}
 		return user;
-	} 
-	
-	
-	
-	
-	
-	
-	
-	
+	}
 	
 	//COMMENT TEST
 	@GetMapping("user")
