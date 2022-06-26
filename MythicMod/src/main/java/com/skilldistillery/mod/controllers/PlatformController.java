a412613b9b96cbd2fe804ad3951dddf57271ff7f
Nathan Hafley <95002066+Nateomancer@users.noreply.github.com>
2022-06-25 18:56:31 -0600

    finished platform controller, service, and repo

--- MythicMod/src/main/java/com/skilldistillery/mod/controllers/PlatformController.java
+++ MythicMod/src/main/java/com/skilldistillery/mod/controllers/PlatformController.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -96,6 +97,22 @@ public class PlatformController {
 			platform = null;
 		}
 		return platform;
+
+	}
+	
+	@DeleteMapping("platform/{id}")
+	public void destroy(HttpServletRequest req, HttpServletResponse res, @PathVariable int id, Principal principal) {
+
+		try {
+			if (platServ.destroy(id)) {
+				res.setStatus(204);
+			} else {
+				res.setStatus(404);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			res.setStatus(400);
+		}
 
 	}
 
